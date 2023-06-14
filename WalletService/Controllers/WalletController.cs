616001fa422f3685e383a67c19a357df0fd58d7a
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2023-06-14 17:39:22 +0000

    commit/added some error handling to the controller

--- WalletService/Controllers/WalletController.cs
+++ WalletService/Controllers/WalletController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WalletService.Repositories;
@@ -26,66 +27,94 @@ namespace WalletService.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<WalletInsertDto>> GetWallets()
         {
-            var wallet = repo.GetWallets();
-            return Ok(dbMapper.Map<IEnumerable<WalletMainDto>>(wallet));
+            try
+            {
+                var wallet = repo.GetWallets();
+                return Ok(dbMapper.Map<IEnumerable<WalletMainDto>>(wallet));
+            }
+            catch (System.Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Internal Server Error");
+            }
         }
 
         [HttpGet]
         [Route("{id:guid}")]
         public ActionResult<WalletMainDto> GetWalletById([FromRoute] Guid id)
         {
-            var wallet = repo.GetWalletById(id);
-            if (wallet == null) return NotFound();
+            try
+            {
+                var wallet = repo.GetWalletById(id);
+                if (wallet == null) return NotFound();
 
-            return Ok(dbMapper.Map<WalletMainDto>(wallet));
+                return Ok(dbMapper.Map<WalletMainDto>(wallet));
+            }
+            catch (System.Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Internal Server Error");
+            }
         }
 
         [HttpPost]
         public async Task<ActionResult<WalletMainDto>> CreateWallet(WalletCreateDto payload)
         {
-            string hashedNumber = new HashValues(payload.AccountNumber).Compute();
-            // Check uniqueness of accountNumber
-            var walletExist = await repo.WalletsExist(hashedNumber);
-            if (walletExist) return BadRequest(new { message = "A wallet with this account number already exist", errorCode = 400 });
-
-            if (payload.Type == "card" && payload.AccountNumber.Length >= 6)
-                payload.AccountNumber = payload.AccountNumber.Substring(0, 6); // cut the first 6 digits
-
-            // Get all wallets of account holder and make sure user does not have more than 5 wallets
-            var total = repo.TotalWalletsOwned(payload.Owner);
-            if (total >= 5) return BadRequest(new { message = "User has more than 5 wallets", errorCode = 400 });
-
-            WalletInsertDto newPayload = new WalletInsertDto()
+            try
             {
-                Name = payload.Name,
-                Type = payload.Type,
-                AccountNumber = payload.AccountNumber,
-                AccountScheme = payload.AccountScheme,
-                AccountHash = hashedNumber,
-                Owner = payload.Owner,
-                Id = Guid.NewGuid(),
-                CreatedAt = DateTime.Now
-            };
+                string hashedNumber = new HashValues(payload.AccountNumber).Compute();
+                // Check uniqueness of accountNumber
+                var walletExist = await repo.WalletsExist(hashedNumber);
+                if (walletExist) return BadRequest(new { message = "A wallet with this account number already exist", errorCode = 400 });
 
-            var newWallet = dbMapper.Map<Wallet>(newPayload);
-            repo.CreateWallet(newWallet);
-            var saved = repo.SaveChanges();
-            if (!saved)
-            {
-                return BadRequest();
+                if (payload.Type == "card" && payload.AccountNumber.Length >= 6)
+                    payload.AccountNumber = payload.AccountNumber.Substring(0, 6); // cut the first 6 digits
+
+                // Get all wallets of account holder and make sure user does not have more than 5 wallets
+                var total = repo.TotalWalletsOwned(payload.Owner);
+                if (total >= 5) return BadRequest(new { message = "User has more than 5 wallets", errorCode = 400 });
+
+                WalletInsertDto newPayload = new WalletInsertDto()
+                {
+                    Name = payload.Name,
+                    Type = payload.Type,
+                    AccountNumber = payload.AccountNumber,
+                    AccountScheme = payload.AccountScheme,
+                    AccountHash = hashedNumber,
+                    Owner = payload.Owner,
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now
+                };
+
+                var newWallet = dbMapper.Map<Wallet>(newPayload);
+                repo.CreateWallet(newWallet);
+                var saved = repo.SaveChanges();
+                if (!saved)
+                {
+                    return BadRequest();
+                }
+
+                var result = dbMapper.Map<WalletMainDto>(newWallet);
+                return Ok(result);
             }
-
-            var result = dbMapper.Map<WalletMainDto>(newWallet);
-            return Ok(result);
+            catch (System.Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Internal Server Error");
+            }
         }
 
         [HttpDelete]
         [Route("{id:guid}")]
         public ActionResult RemoveWallet([FromRoute] Guid id)
         {
-            var done = repo.DeleteWallet(id);
-            if (done) return Ok();
-            return NotFound();
+            try
+            {
+                var done = repo.DeleteWallet(id);
+                if (done) return Ok();
+                return NotFound();
+            }
+            catch (System.Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Internal Server Error");
+            }
         }
     }
 }
